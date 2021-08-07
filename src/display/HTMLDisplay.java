b64f84e0c0b408965144c40d87431dedc4dc2ef4
Benjamin Nelson <33214860+bnelson1324@users.noreply.github.com>
2021-08-07 12:53:34 -0700

    added maven and javalin

--- src/display/HTMLDisplay.java
+++ src/display/HTMLDisplay.java
@@ -1,9 +1,25 @@
 package display;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class HTMLDisplay {
 
+	private static int port = 8080;
+	
 	public static void init() {
 		
+		try {
+			Desktop.getDesktop().browse(new URI("http://localHost:" + port));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
